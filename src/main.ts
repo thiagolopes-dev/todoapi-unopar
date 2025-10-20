60aac9019a07e36d8c18aede00676ec7a80d14ce
Thiago Lopes <thiagoolopezz@gmail.com>
2025-10-20 20:56:51 -0300

    cors config

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
+      app.enableCors({
+        origin: ['http://localhost:4200', 'https://your-frontend-domain.com'], // Allowed origins
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+        credentials: true, // Allow cookies or authentication tokens
+        allowedHeaders: 'Content-Type, Accept, Authorization', // Allowed request headers
+      });
   await app.listen(3000);
 }
 bootstrap();
